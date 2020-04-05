ca6e5a20d5c4535cc820abe21b2abd1b487a0991
huynonstop <tthuykh99@gmail.com>
2020-04-05 13:31:12 +0700

    init redux

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './action'
+
+const initialState = {
+    ingredients: null,
+    totalprice: 4
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+
+        default:
+            return state
+    }
+}
